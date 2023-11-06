057d42346bda1f5bb9b26b26e90ee0f367f1d11e
Walid <oustad.walid@gmail.com>
2023-11-06 15:56:10 +0100

    modif walid

--- test_git_hub.js
+++ test_git_hub.js
@@ -1,12 +1,15 @@
-Coucou hello
-bonjour la famille
-Bien ou quoi 
-aller commençons l'histoire
+"Coucou hello"
+"bonjour la famille"
+"Bien ou quoi "
+"aller commençons l'histoire"
 
-Il était une fois un gros bâteau, avec trois pirates à bord.
-Ils virent au loin un aigle royal 
-Puis l'un d'eux se rendit compte qu'il s'agissait en réalité 
-de la mère du capitaine qui faisait du parapente...
-Il s'esclaffa jusqu'à ce qu'il aperçoive une tornade géante se 
-diriger droit sur elle !
-Le capitain sortit son canon
+"Il était une fois un gros bâteau, avec trois pirates à bord."
+"Ils virent au loin un aigle royal" 
+Puis l'un d'eux se rendit compte qu'il s'agissait en réalité"
+de la mère du capitaine qui faisait du parapente...""
+"Il s'esclaffa jusqu'à ce qu'il aperçoive une tornade géante se 
+diriger droit sur elle !"
+Le capitain sortit son canon"
+
+
+const ADA = 'école'
